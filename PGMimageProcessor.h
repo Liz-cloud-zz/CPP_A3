2d6fd768c915fff865ee9d52ec623b9784d21563
Liz-cloud <MFNLIN003@myuct.ac.za>
2022-03-29 22:41:49 +0200

    PGMimageProcessor.h  file

--- PGMimageProcessor.h
+++ PGMimageProcessor.h
@@ -10,6 +10,11 @@
 #include "ConnectedComponent.h"
 
 namespace MFNLIN003{
+    /**
+     * @brief encapsulates the functionality needed to read
+              in a PGM image and apply a connected component analysis to that image.
+     * 
+     */
     class PGMimageProcessor{
         private:
             //container storing smart pointers to each ConnectedComponent
@@ -20,6 +25,14 @@ namespace MFNLIN003{
             int max;// maximum intensity in image
             std::string comment ="";//image comment
             std::string version;//image version
+        unsigned char *readFile(std::string filename);//read file contents from file
+        bool writeComponents(const std::string & outFileName);//write data to file
+        int extractComponents(unsigned char threshold, int minValidSize);//extract cconnected commponents
+        int filterComponentsBySize(int minSize, int maxSize);//contains current components
+        int getComponentCount(void) const;// get number of pixels of largested component
+        int getLargestSize(void) const;// return number of pixels in largest component
+        int getSmallestSize(void) const;// return number of pixels in smallest component
+        void printComponentData(const ConnectedComponent & theComponent) const;//print the data for a component
 
     };
 }
